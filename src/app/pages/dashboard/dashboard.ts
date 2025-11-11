3b80d2595756e996efbbf43ded5bd30246350134
Sergio Izquierdo <seizquie@gmail.com>
2025-11-11 23:37:30 +0100

    Limpio template

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
-import { NotificationsWidget } from './components/notificationswidget';
-import { StatsWidget } from './components/statswidget';
-import { RecentSalesWidget } from './components/recentsaleswidget';
-import { BestSellingWidget } from './components/bestsellingwidget';
-import { RevenueStreamWidget } from './components/revenuestreamwidget';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [StatsWidget, RecentSalesWidget, BestSellingWidget, RevenueStreamWidget, NotificationsWidget],
+    imports: [],
     template: `
         <div class="grid grid-cols-12 gap-8">
-            <app-stats-widget class="contents" />
-            <div class="col-span-12 xl:col-span-6">
-                <app-recent-sales-widget />
-                <app-best-selling-widget />
+            <div class="col-span-12">
+                <div class="card">
+                    <h5>Dashboard</h5>
+                    <p>Bienvenido a AhorroLand. Este es tu panel de control.</p>
+                </div>
             </div>
-            <div class="col-span-12 xl:col-span-6">
-                <app-revenue-stream-widget />
-                <app-notifications-widget />
-            </div>
+            
+            <!-- Aquí puedes agregar tus widgets personalizados -->
         </div>
     `
 })
